f8dbee9471b0cd13abf6f97ab179b07a0440fdff
ioott <vioott@gmail.com>
2022-11-22 15:52:02 -0300

    test: cria os testes da camada service de motorcycles

--- tests/unit/Services/motorcycles.test.ts
+++ tests/unit/Services/motorcycles.test.ts
@@ -9,7 +9,7 @@ describe('Testa a rota motorcycles', function () {
   describe('Testa a rota do tipo post', function () {
     it('Deve criar uma nova moto com sucesso', async function () {
       const motorcycleInput: IMotorcycle = {
-        model: 'Honda Cb 600f Hornet',
+        model: 'Honda Hornet',
         year: 2005,
         color: 'Yellow',
         status: true,
@@ -29,104 +29,105 @@ describe('Testa a rota motorcycles', function () {
     });
   });
 
-  //   describe('Testa as rotas do tipo get', function () {
-  //     it('Deve listar todos os carros com sucesso', async function () {
-  //       const carInput: ICar[] = [
-  //         {
-  //           _id: '637a3c57ded98fff8aa8bd43',
-  //           model: 'Carro1',
-  //           year: 2002,
-  //           color: 'Black',
-  //           status: true,
-  //           buyValue: 15.990,
-  //           doorsQty: 4,
-  //           seatsQty: 5,
-  //         },
-  //         {
-  //           _id: '637a3c57ded98fff8aa8bd44',
-  //           model: 'Carro2',
-  //           year: 2002,
-  //           color: 'Black',
-  //           status: true,
-  //           buyValue: 15.990,
-  //           doorsQty: 4,
-  //           seatsQty: 5,
-  //         },
-  //       ];
-  //       const carOutput: Car[] = carInput.map((car: ICar) => new Car(car));
-  //       sinon.stub(Model, 'find').resolves(carInput);
+  describe('Testa as rotas do tipo get', function () {
+    it('Deve listar todos as motos com sucesso', async function () {
+      const motorcycleInput: IMotorcycle[] = [
+        {
+          _id: '637a3c57ded98fff8aa8bd43',
+          model: 'Honda Cb 600f Hornet',
+          year: 2005,
+          color: 'Yellow',
+          status: true,
+          buyValue: 30.000,
+          category: 'Street',
+          engineCapacity: 600,
+        },
+        {
+          _id: '637a3c57ded98fff8aa8bd44',
+          model: 'Honda Cbr 1000rr',
+          year: 2011,
+          color: 'Orange',
+          status: true,
+          buyValue: 59.900,
+          category: 'Street',
+          engineCapacity: 1000,
+        },
+      ];
+      const motorcycleOutput: Motorcycle[] = motorcycleInput
+        .map((motorcycle: IMotorcycle) => new Motorcycle(motorcycle));
+      sinon.stub(Model, 'find').resolves(motorcycleInput);
 
-  //       const service = new CarService();
-  //       const result = await service.findAll();
+      const service = new MotorcycleService();
+      const result = await service.findAll();
 
-  //       expect(result).to.be.deep.equal(carOutput);
-  //     });
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
 
-  //     it('Deve listar o carro de um id específico com sucesso', async function () {
-  //       const carInput: ICar = {
-  //         _id: '637a3c57ded98fff8aa8bd43',
-  //         model: 'Carro1',
-  //         year: 2002,
-  //         color: 'Black',
-  //         status: true,
-  //         buyValue: 15.990,
-  //         doorsQty: 4,
-  //         seatsQty: 5,
-  //       };
-  //       const carOutput: Car = new Car({ ...carInput });
-  //       sinon.stub(Model, 'findOne').resolves(carInput);
+    it('Deve listar a moto de um id específico com sucesso', async function () {
+      const motorcycleInput: IMotorcycle = {
+        _id: '637a3c57ded98fff8aa8bd43',
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+      const motorcycleOutput: Motorcycle = new Motorcycle({ ...motorcycleInput });
+      sinon.stub(Model, 'findOne').resolves(motorcycleInput);
 
-  //       const service = new CarService();
-  //       const result = await service.findById('637a3c57ded98fff8aa8bd43');
+      const service = new MotorcycleService();
+      const result = await service.findById('637a3c57ded98fff8aa8bd43');
 
-  //       expect(result).to.be.deep.equal(carOutput);
-  //     });
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
 
-  //     it('Deve retornar um erro caso o id informado seja inválido', async function () {
-  //       sinon.stub(Model, 'findOne').resolves();
-  //       try {
-  //         const service = new CarService();
-  //         await service.findById('xxxxxx');
-  //       } catch (error) {
-  //         expect((error as Error).message).to.be.equal('Invalid mongo id');
-  //       }
-  //     });
+    it('Deve retornar um erro caso o id informado seja inválido', async function () {
+      sinon.stub(Model, 'findOne').resolves();
+      try {
+        const service = new MotorcycleService();
+        await service.findById('xxxxxx');
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Invalid mongo id');
+      }
+    });
 
-  //     it('Deve retornar um erro caso o id informado não seja encontrado', async function () {
-  //       sinon.stub(Model, 'findOne').resolves();
-  //       try {
-  //         const service = new CarService();
-  //         await service.findById('637a3c57ded98fff8aa8bd00');
-  //       } catch (error) {
-  //         expect((error as Error).message).to.be.equal('Car not found');
-  //       }
-  //     });
+    it('Deve retornar um erro caso o id informado não seja encontrado', async function () {
+      sinon.stub(Model, 'findOne').resolves();
+      try {
+        const service = new MotorcycleService();
+        await service.findById('637a3c57ded98fff8aa8bd00');
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Motorcycle not found');
+      }
+    });
 
-  //     afterEach(function () {
-  //       sinon.restore();
-  //     });
-  //   });
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
 
-  //   describe('Testa s rots do tipo put', function () {
-  //     it('Deve atualizar o carro de um id específico com sucesso', async function () {
-  //       const carInput: ICar = {
-  //         model: 'Carro1',
-  //         year: 2002,
-  //         color: 'White',
-  //         status: true,
-  //         buyValue: 15.990,
-  //         doorsQty: 4,
-  //         seatsQty: 5,
-  //       };
-  //       const carOutput: Car = new Car({ ...carInput });
-  //       sinon.stub(Model, 'findByIdAndUpdate').resolves(carOutput);
+  describe('Testa s rotas do tipo put', function () {
+    it('Deve atualizar a moto de um id específico com sucesso', async function () {
+      const motorcycleInput: IMotorcycle = {
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+      const motorcycleOutput: Motorcycle = new Motorcycle({ ...motorcycleInput });
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleOutput);
 
-  //       const service = new CarService();
-  //       const result = await service.updateById('637a3c57ded98fff8aa8bd43', carInput);
+      const service = new MotorcycleService();
+      const result = await service.updateById('637a3c57ded98fff8aa8bd43', motorcycleInput);
 
-  //       expect(result).to.be.deep.equal(carOutput);
+      expect(result).to.be.deep.equal(motorcycleOutput);
 
-//       sinon.restore();
-//     });
-//   });
+      sinon.restore();
+    });
+  });
 });
